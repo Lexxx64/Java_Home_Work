7e1bc76ce73d5dd8fe758c90f44cfde8421573ea
Lexxx <simankovich@inbox.ru>
2023-03-30 18:21:18 +0400

    completed tasks

--- src/Lesson1/HomeWork.java
+++ src/Lesson1/HomeWork.java
@@ -19,15 +19,18 @@ public class HomeWork {
         int year = s.nextInt();
         System.out.println(isLeapYear(year));
 
-        printArray(createArray(5, 15));
-//
-//        int[][] array = new int[4][4]; // [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, x], [0, 0, 0, 1]]
-//        int x = array[2][3];
+        printArray(createArray(5, 15)); // 15 15 15 15 15
+
+        int[] myArray = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        printArray(replaceArray(myArray)); // 0 0 1 1 0 1 0 0 1 1
+
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        printArray(multiplyArray(arr)); // 2 10 6 4 11 8 10 4 8 8 9 2
+
+        newArray(5);
+
     }
-    /**
-     * 1. Написать метод, принимающий на вход два целых числа и проверяющий,
-     * что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
-     */
+
     private static boolean isSumBetween10And20(int a, int b) {
         int c = Integer.sum(a, b);
         if (10 <= c && c <= 20) return true;
@@ -35,7 +38,7 @@ public class HomeWork {
     }
 
     private static boolean isPositive(int x) {
-        if(x > 0) return true;
+        if (x > 0) return true;
         return false;
     }
 
@@ -50,28 +53,53 @@ public class HomeWork {
 
     private static int[] createArray(int len, int initialValue) {
         int[] myArray = new int[len];
-        for (int i = 0; i < myArray.length; i++){
+        for (int i = 0; i < myArray.length; i++) {
             myArray[i] = initialValue;
         }
         return myArray;
     }
 
-    private static void printArray(int[] myArray){
-        for (int i = 0; i < myArray.length; i++){
+    // метод печати одномерного массива
+    private static void printArray(int[] myArray) {
+        for (int i = 0; i < myArray.length; i++) {
             System.out.print(myArray[i] + " ");
         }
         System.out.println();
     }
 
-/**
- * 1. Задать целочисленный массив, состоящий из элементов 0 и 1.
- * Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
- *
- * 2. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
- *
- * 3. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
- * и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно).
- * Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
- * * Также заполнить элементы побочной диагонали
- */
+//    // метод печати двумерного массива
+//    private static void print2DArray(int[][] myArray){
+//        for (int i = 0; i < myArray.length; i++){
+//            for (int j = 0; j < myArray[i].length; j++){
+//                System.out.println(myArray[i][j] + " ");
+//            }
+//        }
+//        System.out.println();
+//    }
+
+    private static int[] replaceArray(int[] myArray){
+        for (int i = 0; i < myArray.length; i++){
+            myArray[i] = (myArray[i] == 1) ? 0 : 1;
+        }return myArray;
+    }
+
+    private static int[] multiplyArray(int[] myArray){
+        for (int i = 0; i < myArray.length; i++){
+            if (myArray[i] < 6) myArray[i] *= 2;
+        }return myArray;
+    }
+
+    private static void newArray(int len){
+        int [][] newArray = new int[len][len];
+        for (int i = 0; i < newArray.length; i++){
+            for (int j = 0; j < newArray[i].length; j++){
+                if (i + j + 1 == len || i == j){
+                    newArray[i][j] = 1;
+                }
+                else {
+                    newArray[i][j] = 0;
+                }
+            }
+        }System.out.println(Arrays.deepToString(newArray).replace("], ", "]\n"));
+    }
 }
