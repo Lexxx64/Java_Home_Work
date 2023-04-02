772c7fd5c4c52003599471522f491b3b7f2843d1
Lexxx <simankovich@inbox.ru>
2023-04-02 21:01:39 +0400

    method remove implemented

--- src/Lesson3/HomeWork.java
+++ src/Lesson3/HomeWork.java
@@ -1,4 +1,21 @@
 package Lesson3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeWork {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        strings.add("string");
+        strings.add("40");
+        strings.add("-5");
+        strings.add("my_string");
+        removeIntegers(strings);
+        System.out.println(strings); // [string, my_string]
+    }
+
+    private static void removeIntegers(List<String> strings) {
+        strings.removeIf(e -> (strings.indexOf(e) % 2) == 0);
+    }
+
 }
