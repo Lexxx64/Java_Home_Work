df093f850b5a8ae4375c793fa0b55f246036e8cb
Lexxx <simankovich@inbox.ru>
2023-04-11 11:40:34 +0400

    method remove implemented

--- src/Lesson3/HomeWork.java
+++ src/Lesson3/HomeWork.java
@@ -2,6 +2,7 @@ package Lesson3;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class HomeWork {
     public static void main(String[] args) {
@@ -12,10 +13,31 @@ public class HomeWork {
         strings.add("my_string");
         removeIntegers(strings);
         System.out.println(strings); // [string, my_string]
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(40);
+        numbers.add(-5);
+        numbers.add(-15);
+        removeEvenNumber(numbers);
+        System.out.println(numbers); // [40, -15]
     }
 
-    private static void removeIntegers(List<String> strings) {
-        strings.removeIf(e -> (strings.indexOf(e) % 2) == 0);
+    private static void removeEvenNumber(List<Integer> numbers) {
+        numbers.removeIf(e -> (numbers.indexOf(e) % 2) == 0);
     }
 
+    static boolean stringConv(String strings){
+        try{
+            Integer.parseInt(strings);
+            return true;
+        }catch (NumberFormatException e) {
+        }
+        return false;
+    }
+
+
+   static void removeIntegers(List<String> strings) {
+        strings.removeIf(e -> stringConv(e));
+    }
 }
