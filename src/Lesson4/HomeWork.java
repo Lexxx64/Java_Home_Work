aae4fe7d51f0ee4c8f23f55221ee8a77bebaeb6d
Lexxx <simankovich@inbox.ru>
2023-04-24 18:07:56 +0400

    rewrote the program

--- src/Lesson4/HomeWork.java
+++ src/Lesson4/HomeWork.java
@@ -3,18 +3,26 @@ package Lesson4;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class HomeWork {
     public static void main(String[] args) {
+        Deque<String> deque = new LinkedList<>();
         Scanner sc = new Scanner(System.in);
-        Deque<String> inputList = new ArrayDeque<>();
         while (true) {
-            String input;
-            while (!(input = sc.nextLine()).equals("print"))
-                inputList.addFirst(input);
-            inputList.removeIf(x -> inputList.remove("revert"));
-            System.out.println(inputList);
+            String input = sc.nextLine();
+            if ("exit".equals(input))
+                break;
+            if ("print".equals(input)) {
+                System.out.println(deque);
+            } else if ("revert".equals(input)) {
+                if (!deque.isEmpty()) {
+                    deque.removeFirst();
+                }
+            } else {
+                deque.addFirst(input);
+            }
         }
     }
 }
--- src/Lesson4/Seminar.java
+++ src/Lesson4/Seminar.java
@@ -1,17 +1,9 @@
 package Lesson4;
 
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Seminar {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        List<String[]> storage = new LinkedList<>();
-        String input = sc.nextLine();
-        String[] pair =  input.split("~");
-        if (pair.length != 2){
-            throw new IllegalArgumentException();
-        }
-    }
+
 }
